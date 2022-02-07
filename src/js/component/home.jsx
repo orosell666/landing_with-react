e0709514ec9d5f46d499b706e9fdf8a9a053764c
Oriol Rosell <o.rosell.ribas@gmail.com>
2022-02-07 21:45:37 +0100

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+import NaviBar from "./NaviBar.jsx";
+import Jumbo from "./Jumbo.jsx";
+import Cards from "./Cards.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<NaviBar />
+			<div className="container">
+				<Jumbo />
+				<div className="in-flex">
+					<Cards />
+					<Cards />
+					<Cards />
+					<Cards />
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
